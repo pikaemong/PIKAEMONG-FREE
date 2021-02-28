1aae2cebf3f012c95037612939a282d8f5fb8331
피카에몽 <78637281+pikaemong@users.noreply.github.com>
2021-02-28 17:07:00 +0900

    Add files via upload

---
+++ 디스코드 봇 패널/locales.js
@@ -0,0 +1,136 @@
+const locales = {
+    "kr": {
+        "cCode": "kr",
+        "language": "한국어(Korean)",
+        "text": {
+            "channelNameLabel": "채팅",
+            "scrollCheck": "자동 스크롤 애니메이션 사용",
+            "fileSent": "파일을 (s) 개 보냈다.",
+            "attachmentTxt": "첨부파일",
+            "privateMessages": "봇 DM",
+            "userId": "사용자 ID"
+        },
+        "buttons": {
+            "clearLastMessages": "메시지 기록 초기화",
+            "editToken": "토큰 변경",
+            "refreshChat": "채팅 새로고침",
+            "changeLanguage": "언어 변경",
+            "send": "전송",
+            "leave": "서버를 나갔습니다.",
+            "invite": "초대링크 생성"
+        },
+        "infos": {
+            "owner": "주인",
+            "members": "맴버",
+            "vChannels": "음성채널",
+            "tChannels": "메시지채널",
+            "roles": "역할",
+            "channels": "채널",
+            "emojis": "이모지"
+        },
+        "headings": {
+            "lastMessages": "메시지 기록",
+            "guildName": "정보",
+            "autoScroll": "자동 옵션",
+        },
+        "errors": {
+            "error": "에러",
+            "dm": "오류 : 디엠",
+            "emptyMsg": "오류 : 비어있는 메시지",
+            "missingPermissions": "오류 : 권한 부족"
+        },
+        "token": {
+            "prompt": "봇 토큰을 추가해주세요.",
+            "confirmation": "진행 하시겠습니까?",
+            "invalid": "토큰이 없거나, 올바르지 않습니다."
+        },
+        "messageType": {
+            "serverJoin": "서버에 입장했습니다.",
+            "pin": "메시지를 고정",
+            "channelNews": "원격 뉴스 채널 추가",
+            "boost": "서버에 부스트를 했습니다."
+        },
+        "fileType": {
+            "unknown": "알수없음",
+            "img": "이미지",
+            "doc": "문서",
+            "pdf": "pdf파일",
+            "video": "영상",
+            "audio": "오디오"
+        },
+        "formatting": {
+            "bold": "굵게",
+            "emphasis": "기울기",
+            "underline": "밑줄",
+            "strike": "글 위에 줄",
+            "clear": "지우개"
+        }
+    },
+	"en": {
+        "cCode": "en",
+        "language": "영어(English)",
+        "text": {
+            "channelNameLabel": "Chat",
+            "scrollCheck": "Enable auto-scrolling animation",
+            "fileSent": "sent some file(s)",
+            "attachmentTxt": "Attachments",
+            "privateMessages": "DM",
+            "userId": "User ID"
+        },
+        "buttons": {
+            "clearLastMessages": "Clear Last Messages",
+            "editToken": "Edit token",
+            "refreshChat": "Refresh chat",
+            "changeLanguage": "Change language",
+            "send": "Send",
+            "leave": "Leave the server",
+            "invite": "Invite"
+        },
+        "infos": {
+            "owner": "Owner",
+            "members": "Members",
+            "vChannels": "Channels (voice)",
+            "tChannels": "Channels (text)",
+            "roles": "Roles",
+            "channels": "Channels",
+            "emojis": "Emojis"
+        },
+        "headings": {
+            "lastMessages": "Last Messages",
+            "guildName": "Information",
+            "autoScroll": "Auto-Scroll",
+        },
+        "errors": {
+            "error": "ERROR",
+            "dm": "ERROR : DM",
+            "emptyMsg": "ERROR : EMPTY MESSAGE",
+            "missingPermissions": "ERROR : MISSING PERMISSIONS"
+        },
+        "token": {
+            "prompt": "Please enter your discord bot token",
+            "confirmation": "Are you sure ?",
+            "invalid": "No token provided or token is invalid"
+        },
+        "messageType": {
+            "serverJoin": "joined the server",
+            "pin": "pinned a message",
+            "channelNews": "added a remote channel news",
+            "boost": "boosted the server"
+        },
+        "fileType": {
+            "unknown": "unknown",
+            "img": "image",
+            "doc": "document",
+            "pdf": "pdf",
+            "video": "video",
+            "audio": "audio"
+        },
+        "formatting": {
+            "bold": "Bold",
+            "emphasis": "Emphasis",
+            "underline": "Underline",
+            "strike": "Strikethrough",
+            "clear": "Clear formatting"
+        }
+    }
+};
